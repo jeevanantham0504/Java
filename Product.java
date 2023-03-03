c216089578e48b9fda0dd919c76cdb990485c9d5
jeevanantham <124482848+jeevanantham0504@users.noreply.github.com>
2023-03-03 21:30:00 +0530

    Product of two  numbers

---
+++ Product.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Product{
+
+public static void main(String[] args){
+
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Enter the value of a: ");
+	int a = scan.nextInt();
+
+	System.out.println("Enter the value of b: ");
+	int b = scan.nextInt();
+
+	int c = a*b;
+	System.out.println("The product of "+a+" and "+b+" are "+c );
+	
+
+
+
+
+
+}
+}
