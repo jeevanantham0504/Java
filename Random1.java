422abb93885e18f2383ecde90721da4b06e0472c
jeevanantham <124482848+jeevanantham0504@users.noreply.github.com>
2023-03-04 00:49:33 +0530

    to print random numbers

---
+++ Random1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Random1 {
+
+ public static void main(String[] args) {
+  
+  Random input = new Random();
+  
+  int x = input.nextInt(6)+1;
+  //double y = input.nextDouble();
+  //boolean z = input.nextBoolean();
+  
+  System.out.println(x);
+    
+ }
+}
