4b78830fe518efdb5d7cc071a5f8bfffb372ecee
jeevanantham <124482848+jeevanantham0504@users.noreply.github.com>
2023-03-03 23:56:51 +0530

    program to find the average of 5 numbers

---
+++ Average1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Average1{
+public static void main(String[] args){
+
+	Scanner avg = new Scanner(System.in);
+	int a,b,c,d,e,average;
+	System.out.println("Enter five values:");
+	a = avg.nextInt();
+	b = avg.nextInt();
+	c = avg.nextInt();
+	d = avg.nextInt();
+	e = avg.nextInt();
+
+	average=(a+b+c+d+e)/5;
+
+	System.out.println("The average of "+a+" "+b+" "+c+" "+d+" "+e+" "+"is"+" "+average);
+
+	
+	
+
+	
+		
+
+
+
+
+
+
+}
+}
