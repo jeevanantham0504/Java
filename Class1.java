087bbae9e007100739e77286fd56bc7db7440746
jeevanantham <124482848+jeevanantham0504@users.noreply.github.com>
2023-03-10 10:00:13 +0530

    package files

---
+++ Class1.java
@@ -0,0 +1,21 @@
+package com.jeevanantham;
+
+public class Class1{
+
+public void method1(){
+	
+System.out.println("Calling method one from class one");
+
+}
+
+public static void main(String[] args){
+
+Class1 jeeva = new Class1();
+
+	jeeva.method1();
+
+}
+	
+	
+}
+
